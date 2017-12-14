cb60378e285739e84066476604cc25b188f48e74
Eric Cooper <ecc@cmu.edu>
2017-12-13 21:13:27 -0500

    Add program to check subg_rfspy version and state

---
+++ cmd/probe/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ecc1/cc111x"
+)
+
+func main() {
+	r := cc111x.Open()
+	r.Flush()
+	if r.Error() != nil {
+		log.Fatal(r.Error())
+	}
+	fmt.Printf("version: %s\n", r.Version())
+	fmt.Printf("state: %s\n", r.State())
+	fmt.Printf("old frequency: %d\n", r.Frequency())
+	r.SetFrequency(900000000)
+	fmt.Printf("new frequency: %d\n", r.Frequency())
+}
