175c16bee01f7e52ba8a75ed9ecc4ad168be5672
Eric Cooper <ecc@cmu.edu>
2017-12-15 22:11:31 -0500

    Add marshaling tests

---
+++ marshal_test.go
@@ -0,0 +1,41 @@
+package cc111x
+
+import (
+	"bytes"
+	"math"
+	"testing"
+)
+
+func TestMarshalUint16(t *testing.T) {
+	cases := []struct {
+		val uint16
+		rep []byte
+	}{
+		{0x1234, []byte{0x12, 0x34}},
+		{0, []byte{0, 0}},
+		{math.MaxUint16, []byte{0xFF, 0xFF}},
+	}
+	for _, c := range cases {
+		rep := marshalUint16(c.val)
+		if !bytes.Equal(rep, c.rep) {
+			t.Errorf("marshalUint16(%04X) == % X, want % X", c.val, rep, c.rep)
+		}
+	}
+}
+
+func TestMarshalUint32(t *testing.T) {
+	cases := []struct {
+		val uint32
+		rep []byte
+	}{
+		{0x12345678, []byte{0x12, 0x34, 0x56, 0x78}},
+		{0, []byte{0, 0, 0, 0}},
+		{math.MaxUint32, []byte{0xFF, 0xFF, 0xFF, 0xFF}},
+	}
+	for _, c := range cases {
+		rep := marshalUint32(c.val)
+		if !bytes.Equal(rep, c.rep) {
+			t.Errorf("marshalUint32(%08X) == % X, want % X", c.val, rep, c.rep)
+		}
+	}
+}
