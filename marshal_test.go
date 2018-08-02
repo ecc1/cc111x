b130cfaeb288d93f40757a06b2da672b7cc4f8ae
Eric Cooper <ecc@cmu.edu>
2018-08-02 13:48:04 -0400

    Use subtest for each test case

--- marshal_test.go
+++ marshal_test.go
@@ -2,6 +2,7 @@ package cc111x
 
 import (
 	"bytes"
+	"fmt"
 	"math"
 	"testing"
 )
@@ -16,10 +17,12 @@ func TestMarshalUint16(t *testing.T) {
 		{math.MaxUint16, []byte{0xFF, 0xFF}},
 	}
 	for _, c := range cases {
-		rep := marshalUint16(c.val)
-		if !bytes.Equal(rep, c.rep) {
-			t.Errorf("marshalUint16(%04X) == % X, want % X", c.val, rep, c.rep)
-		}
+		t.Run(fmt.Sprintf("marshal16_%d", c.val), func(t *testing.T) {
+			rep := marshalUint16(c.val)
+			if !bytes.Equal(rep, c.rep) {
+				t.Errorf("marshalUint16(%04X) == % X, want % X", c.val, rep, c.rep)
+			}
+		})
 	}
 }
 
@@ -33,9 +36,11 @@ func TestMarshalUint32(t *testing.T) {
 		{math.MaxUint32, []byte{0xFF, 0xFF, 0xFF, 0xFF}},
 	}
 	for _, c := range cases {
-		rep := marshalUint32(c.val)
-		if !bytes.Equal(rep, c.rep) {
-			t.Errorf("marshalUint32(%08X) == % X, want % X", c.val, rep, c.rep)
-		}
+		t.Run(fmt.Sprintf("marshal32_%d", c.val), func(t *testing.T) {
+			rep := marshalUint32(c.val)
+			if !bytes.Equal(rep, c.rep) {
+				t.Errorf("marshalUint32(%08X) == % X, want % X", c.val, rep, c.rep)
+			}
+		})
 	}
 }
